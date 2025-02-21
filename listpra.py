9076f50bf4b3f4aa58a48aeaaeff4181f15227ac
Jay <jayshreekant@gmail.com>
2025-02-22 03:01:26 +0530

    tesdrt

--- listpra.py
+++ listpra.py
@@ -11,3 +11,14 @@ print(list1.count(4))
 
 list2 = [i for i in range(50) if i%4 ==0]
 print(list2)
+
+list3 = [3,22,55,21,112,1,2,89,4,7,545,4,45,5]
+print(list3)
+list3.sort()
+print(list3)
+print(list3.sort())
+list3.sort(reverse=True)
+print(list3)
+list3.sort()
+print(list3)
+# print(sorted(list3))
